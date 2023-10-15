9bb5caaaaae47c884171858aa9bd6379faa61f58
Sejal <sejalpradhan290@gmail.com>
2023-10-15 18:56:54 +0530

    Updates

---
+++ precache-manifest.239257c409fdbe632fa51720c16f8a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db05b29947c537232aaca60cdb65f31",
+    "url": "/mobile-calculator/index.html"
+  },
+  {
+    "revision": "ba033111a20ff8321580",
+    "url": "/mobile-calculator/static/css/main.efb92317.chunk.css"
+  },
+  {
+    "revision": "55d9828ca1827bb8cae8",
+    "url": "/mobile-calculator/static/js/2.a5d8ed2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mobile-calculator/static/js/2.a5d8ed2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba033111a20ff8321580",
+    "url": "/mobile-calculator/static/js/main.f9ef44c8.chunk.js"
+  },
+  {
+    "revision": "0157118196f3eee47575",
+    "url": "/mobile-calculator/static/js/runtime-main.5b940ee7.js"
+  }
+]);
